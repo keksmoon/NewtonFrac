9bff43751a0ef3d54b9e485017b05f14fa4b3e9b
Tim <timofei.teterin@inbox.ru>
2021-05-19 00:04:48 +0300

    Добавьте файлы проекта.

---
+++ NewtonFrac/App.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewtonFrac {
+    public partial class App : Form {
+        public App() {
+            InitializeComponent();
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            Pen myPen = new Pen(Color.Red, 1);
+            var bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bitmap;
+            Graphics g = Graphics.FromImage(bitmap);
+
+
+            Fractal f = new Fractal(pictureBox1.Width, pictureBox1.Height, 0.0005, 50);
+            var res = f.Draw();
+
+            for (int i = 0; i < pictureBox1.Height; i++) {
+                for (int j = 0; j < pictureBox1.Width; j++) {
+                    myPen.Color = Color.FromArgb(res[j, i], res[j, i], 0);
+                    g.DrawRectangle(myPen, j, i, 1, 1);
+                }
+            }
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            pictureBox1.Image.Save($"image{DateTime.Now.Day}-{DateTime.Now.Month}-{DateTime.Now.Year}" +
+                $"-{DateTime.Now.Hour}-{DateTime.Now.Minute}-{DateTime.Now.Millisecond}.png", ImageFormat.Png);
+        }
+    }
+
+    public class Fractal {
+        public int M, N; // Размеры поля для отрисовки фрактала
+        public double Rmin; 
+        public int Kmax;
+
+        public int[,] Draw() {
+            int[,] FracField = new int[M, N];
+
+            Complex z, t;
+
+            int mx = M / 2;
+            int my = N / 2;
+
+            for (int y = -my; y < my; y++) {
+                for (int x = -mx; x < mx; x++) {
+                    z = new Complex(x * 0.005, y * 0.005);
+
+                    double d = 1; // Радиус сходимости
+
+                    int n = 0;
+                    while (d > Rmin && n < Kmax) {
+                        t = z;
+
+                        int k = 6;
+
+                        // x^k-1
+                        z = z - (z.Pow(k) - 1) / (k * z.Pow(k - 1));
+
+                        // x^k+z^k/2+1
+                        //z = z - (1 + z.Pow(k) + z.Pow(k / 2)) / (k * z.Pow(k - 1) + (k / 2) * z.Pow(k / 2 - 1));
+
+                        d = (t - z).Mod().Abs();
+                        n++;
+                    }
+
+                    int col = (n * 9) % 255;
+                    FracField[mx + x, my + y] = col;
+                }
+            }
+
+            return FracField;
+        }
+
+        public Fractal(int _M, int _N, double _Rmin, int _Kmax) {
+            M = _M;
+            N = _N;
+            Rmin = _Rmin;
+            Kmax = _Kmax;
+        }
+    }
+    public class Complex {
+        public double Re, Im;
+
+        public static Complex operator -(Complex first, Complex second) {
+            return new Complex(first.Re - second.Re, first.Im - second.Im);
+        }
+
+        public static Complex operator +(Complex first, Complex second) {
+            return new Complex(first.Re + second.Re, first.Im + second.Im);
+        }
+
+        public static Complex operator *(Complex first, Complex second) {
+            return new Complex(first.Re * second.Re - first.Im * second.Im, first.Re * second.Im + first.Im * second.Re);
+        }
+
+        public static Complex operator /(Complex first, Complex second) {
+            return new Complex((second.Re * first.Re + second.Im * first.Im) / (second.Re * second.Re + second.Im * second.Im),
+                (second.Re * first.Im - second.Im * first.Re) / (second.Re * second.Re + second.Im * second.Im));
+        }
+
+        public static Complex operator *(int first, Complex second) {
+            return new Complex(second.Re * first, second.Im * first);
+        }
+
+        public static Complex operator +(int first, Complex second) {
+            return new Complex(second.Re + first, second.Im);
+        }
+
+        public static Complex operator -(Complex first, int second) {
+            return new Complex(first.Re - second, first.Im);
+        }
+
+        public static Complex operator -(int first, Complex second) {
+            return new Complex(first - second.Re, 0 - second.Im);
+        }
+
+        public double Abs() {
+            return Re * Re + Im * Im;
+        }
+
+        public Complex Mod() {
+            return new Complex(Math.Abs(this.Re), Math.Abs(this.Im));
+        }
+
+        public Complex Pow(int k) {
+            var zmul = new Complex(this.Re, this.Im);
+            var z = new Complex(this.Re, this.Im);
+            for (int i = 0; i < k - 1; i++)
+                z *= zmul;
+            return z;
+        }
+
+        public Complex(double _Re, double _Im) {
+            Re = _Re;
+            Im = _Im;
+        }
+
+        public Complex() {
+            Re = 0;
+            Im = 0;
+        }
+    }
+}
---
+++ NewtonFracFSharp/Program.fs
@@ -0,0 +1,137 @@
+﻿#light
+open System
+open System.Drawing
+open System.Drawing.Imaging
+open System.Windows.Forms
+open Microsoft.FSharp.Math
+
+let Width = 1920
+let Height = 1080
+let bitmap = new Bitmap(Width, Height)
+let g = Graphics.FromImage(bitmap)
+let myPen = new Pen(Color.Black, (float32 1))
+
+let drawRect (x:float) (y:float) (col:int) = 
+    g.DrawRectangle(new Pen(Color.FromArgb((int)col, (int)col, 0)), (int)x, (int)y, 1, 1)
+  
+let mulComplex (comf:complex, coms:complex) =
+    let ReF = comf.RealPart
+    let ImF = comf.ImaginaryPart
+    let ReS = coms.RealPart
+    let ImS = coms.ImaginaryPart
+
+    let zRe = ReF * ReS - ImF * ImS
+    let zIm = ReF * ImS + ImF * ReS
+    complex zRe zIm
+
+let divComplex (comf:complex, coms:complex) = 
+    let ReF = comf.RealPart
+    let ImF = comf.ImaginaryPart
+    let ReS = coms.RealPart
+    let ImS = coms.ImaginaryPart
+
+    let zRe = ((ReS * ReF + ImS * ImF) / (ReS * ReS + ImS * ImS))
+    let zIm = ((ReS * ImF - ImS * ReF) / (ReS * ReS + ImS * ImS))
+    complex zRe zIm
+
+let powComplex (zmul:complex, k:int) =
+    let mutable z = zmul
+    for i in 1 .. k - 1 do 
+        z <- mulComplex(z, zmul)
+    z
+
+let drawFractal (M:float) (N:float) (rad:float) (kmax:float) (alpha:float) (beta:float) =
+    let mx = M / 2.0
+    let my = N / 2.0
+
+    let mutable t = complex 1. 1.
+    let mutable z = complex 1. 1.
+
+    for y in -my .. my do
+        for x in -mx .. mx do
+            z <- complex (x * 0.005)  (y * 0.005)
+
+            let mutable d = 1.
+            let mutable n = 0
+            while ((float) d > rad && (float) n < kmax) do
+                t <- z
+                z <- z - divComplex((powComplex(z, (int)alpha) - complex beta 0.), (mulComplex((complex alpha 0.), powComplex(z, (int)alpha - 1))))
+
+                let m =  t - z
+                d <- (m.RealPart * m.RealPart + m.ImaginaryPart * m.ImaginaryPart)
+
+                n <- n + 1
+
+            drawRect (mx+x) (my+y) ((n * 9) % 255)
+   
+
+let formGet = 
+    let form = new Form(Text = "Newton Fractal")
+    
+    let button = new Button(Text = "Save To Folder")
+    button.Location <- new Point(10, 10)
+    button.Size <- new Size(100, 30)
+    button.Click.Add(fun e ->   
+        let ofd = new SaveFileDialog()
+        ofd.Filter <- "Растровое изображение (*.bmp)|*.bmp"
+        if (ofd.ShowDialog() = DialogResult.OK) then
+            bitmap.Save($"{ofd.FileName}.bmp", ImageFormat.Bmp)
+        else
+            bitmap.Save($"image.bmp", ImageFormat.Bmp)
+    )
+    form.Controls.Add(button)
+
+    let inputAlpha = new TextBox()
+    inputAlpha.Location <- new Point(10, 45)
+    inputAlpha.Size <- new Size(100, 30)
+    inputAlpha.KeyPress.Add(fun e ->
+         let number = e.KeyChar;
+         
+         if (Char.IsDigit(number) = false && (e.KeyChar = char(8)) = false) then
+            e.Handled <- true
+    )
+    inputAlpha.Text <- "3"
+    form.Controls.Add(inputAlpha)
+
+    let inputBeta = new TextBox()
+    inputBeta.Location <- new Point(10, 70)
+    inputBeta.Size <- new Size(100, 30)
+    inputBeta.KeyPress.Add(fun e ->
+         let number = e.KeyChar;
+         
+         if (Char.IsDigit(number) = false && (e.KeyChar = char(8)) = false) then
+            e.Handled <- true
+    )
+    inputBeta.Text <- "1"
+    form.Controls.Add(inputBeta)
+
+    let rebuild = new Button(Text = "Rebuild")
+    rebuild.Location <- new Point(10, 95)
+    rebuild.Size <- new Size(100, 30)
+    rebuild.Click.Add(fun e ->   
+        if (inputAlpha.Text = String.Empty) then
+            inputAlpha.Text <- "1"
+        let inputA = (int)inputAlpha.Text
+
+        if (inputBeta.Text = String.Empty) then
+            inputBeta.Text <- "1"
+        let inputB = (int)inputBeta.Text
+
+        g.Clear(Color.White)
+        drawFractal (float Width) (float Height) 0.0001 50. (float inputA) (float inputB)
+        form.Paint.Add(fun e -> e.Graphics.DrawImage(bitmap, 0, 0))
+        form.Invalidate()
+    )
+    form.Controls.Add(rebuild)
+
+    drawFractal (float Width) (float Height) 0.0001 50. 3. 1.
+
+    form.Paint.Add(fun e -> e.Graphics.DrawImage(bitmap, 0, 0))
+    form
+
+
+
+[<STAThread>]
+do Application.Run(formGet);;
+
+
